6f4a89f713a76907a303d16cca055d00b6572d59
caitlinjillybelle <caitlinjillybelle@gmail.com>
2021-05-07 11:09:45 +1200

    Files updated to newest version.

---
+++ alpha345_00/app/src/main/java/com/example/alpha345_00/GameActivity.kt
@@ -0,0 +1,11 @@
+package com.example.alpha345_00
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GameActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_game2)
+    }
+}
